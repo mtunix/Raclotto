1a451397fb25e6ee5f7b5b8ae9657f9599b04a44
MTardchild <Marius.Tuschl@googlemail.com>
2020-12-31 14:32:00 +0100

    python not good for semicolons

--- raclotto.js
+++ raclotto.js
@@ -1,28 +1,28 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
 var ingredients = [];
-var sauces = []
+var sauces = [];
 
 var types = {
     ingredient: "ingredient",
     sauce: "sauce"
-}
+};
 
-var buttonRemoveText = "Raus aus dem Topf"
+var buttonRemoveText = "Raus aus dem Topf";
 
 window.onload = function () {
     if (vw < 1400) {
-        buttonRemoveText = "X"
+        buttonRemoveText = "X";
     }
 
     if (localStorage.getItem('ingredients')) {
-        ingredients = JSON.parse(localStorage.getItem('ingredients'))
-        populate(types.ingredient)
+        ingredients = JSON.parse(localStorage.getItem('ingredients'));
+        populate(types.ingredient);
     }
 
     if (localStorage.getItem('sauces')) {
-        sauces = JSON.parse(localStorage.getItem('sauces'))
-        populate(types.sauce)
+        sauces = JSON.parse(localStorage.getItem('sauces'));
+        populate(types.sauce);
     }
 
     $('#input-ingredient').keypress(function (event) {
@@ -38,65 +38,65 @@ window.onload = function () {
     });
 
     $('#btn-add-ingredient').click(function (event) {
-        add(types.ingredient)
+        add(types.ingredient);
     });
 
     $('#btn-add-sauce').click(function (event) {
-        add(types.sauce)
+        add(types.sauce);
     });
 
     $('#btn-gimme-food').click(function () {
-        gimmeSomeFood()
+        gimmeSomeFood();
     });
 }
 
 function add(type) {
-    let listContainer = find(type + "-list")
-    let item = find("input-" + type).value
+    let listContainer = find(type + "-list");
+    let item = find("input-" + type).value;
     if (item !== undefined) {
-        window[type + "s"].push(item)
-        listContainer.appendChild(generate(window[type + "s"].length - 1, type, true))
-        find("input-" + type).value = ""
+        window[type + "s"].push(item);
+        listContainer.appendChild(generate(window[type + "s"].length - 1, type, true));
+        find("input-" + type).value = "";
     }
-    localStorage.setItem(type + "s", JSON.stringify(window[type + "s"]))
+    localStorage.setItem(type + "s", JSON.stringify(window[type + "s"]));
 }
 
 function populate(type) {
-    let listContainer = find(type + "-list")
+    let listContainer = find(type + "-list");
     for (let i = 0; i < window[type + "s"].length; ++i) {
-        listContainer.appendChild(generate(i, type, true))
+        listContainer.appendChild(generate(i, type, true));
     }
 }
 
 function remove(type, id) {
     $('#' + type + id).remove();
     window[type + "s"].splice(id - 1, 1);
-	localStorage.setItem(type + "s", JSON.stringify(window[type + "s"]))
+	localStorage.setItem(type + "s", JSON.stringify(window[type + "s"]));
 }
 
 function generate(id, type, allowDelete) {
-    let item = document.createElement("div")
+    let item = document.createElement("div");
     item.innerHTML = window[type + "s"][id];
-    item.id = type + id
-    item.setAttribute("class", "ingredient")
+    item.id = type + id;
+    item.setAttribute("class", "ingredient");
 
     if (allowDelete) {
-        let button = document.createElement("button")
-        button.setAttribute("type", "button")
-        button.setAttribute("class", "btn btn-link btn-sm btn-remove")
-        button.innerHTML = buttonRemoveText
+        let button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "btn btn-link btn-sm btn-remove");
+        button.innerHTML = buttonRemoveText;
         button.addEventListener("click", function () {
             remove(type, id);
         })
-        item.appendChild(button)
+        item.appendChild(button);
     }
 
-    return item
+    return item;
 }
 
 function gimmeSomeFood() {
     let randomIngredients = randomize($('#input-ingredient-count').val(), types.ingredient);
-    let randomSauces = randomize($('#input-sauce-count').val(), types.sauce)
+    let randomSauces = randomize($('#input-sauce-count').val(), types.sauce);
     $('#ingredients-random').empty();
     for (let i = 0; i < randomIngredients.length; ++i)
         $('#ingredients-random').append(generate(randomIngredients[i], types.ingredient, false));
@@ -123,9 +123,3 @@ function randomize(count, type) {
 function find(id) {
     return document.getElementById(id);
 }
-
-function isMobile() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-};
