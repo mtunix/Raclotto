dd942b1c0e3b0c7fe0d8543f9fb0f6b008d9cf1a
mtardchild <marius.tuschl@gmail.com>
2021-12-30 04:14:38 +0100

    ui improvements

--- front/src/components/addIngredient.js
+++ front/src/components/addIngredient.js
@@ -40,7 +40,7 @@ export class AddIngredient extends React.Component {
         Api.add(this.props.session, this.state).then((data) => {
             this.props.onAdd()
         });
-        this.setState({"name":""})
+        this.setState({"name": ""})
     };
 
     onIngredientSelected = (type) => {
@@ -49,60 +49,70 @@ export class AddIngredient extends React.Component {
 
     render() {
         let checks = this.options.map((option) =>
-        <ToggleButton
-            className="mb-2"
-            id={option.name}
-            type="checkbox"
-            variant="outline-primary"
-            checked={this.state[option.name]}
-            onChange={this.setChecked}
-        >
-            {option.localized}
-        </ToggleButton>
+            <ToggleButton
+                className="mb-2"
+                id={option.name}
+                type="checkbox"
+                variant="outline-primary"
+                checked={this.state[option.name]}
+                onChange={this.setChecked}
+            >
+                {option.localized}
+            </ToggleButton>
         );
 
-        return (<Form.Group className="mb-3" controlId="formIngredientName">
-            <Form.Label>Zutatenname</Form.Label>
-            <Form.Control
-                value={this.state.name}
-                onChange={this.onNameChanged}
-                type="name"
-                placeholder="Namen eingeben"
-            />
-            <div className="d-grid gap-2 mt-2">
-                <ButtonGroup>
-                    <ToggleButton
-                        id="ingredient"
-                        type="radio"
-                        variant="outline-primary"
-                        checked={this.state.type === 1}
-                        onChange={() => this.onIngredientSelected(1)}
-                    >
-                        Zutat
-                    </ToggleButton>
-                    <ToggleButton
-                        id="sauce"
-                        type="radio"
-                        variant="outline-primary"
-                        checked={this.state.type === 2}
-                        onChange={() => this.onIngredientSelected(2)}
-                    >
-                        Sauce
-                    </ToggleButton>
-                </ButtonGroup>
+        return (
+            <div>
+                <Form.Group className="mb-3" controlId="formIngredientName">
+                    <Form.Label>Zutatenname</Form.Label>
+                    <Form.Control
+                        value={this.state.name}
+                        onChange={this.onNameChanged}
+                        type="name"
+                        placeholder="Namen eingeben"
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <div className="d-grid">
+                        <Form.Label>Art</Form.Label>
+                        <ButtonGroup>
+                            <ToggleButton
+                                id="ingredient"
+                                type="radio"
+                                variant="outline-primary"
+                                checked={this.state.type === 1}
+                                onChange={() => this.onIngredientSelected(1)}
+                            >
+                                Zutat
+                            </ToggleButton>
+                            <ToggleButton
+                                id="sauce"
+                                type="radio"
+                                variant="outline-primary"
+                                checked={this.state.type === 2}
+                                onChange={() => this.onIngredientSelected(2)}
+                            >
+                                Sauce
+                            </ToggleButton>
+                        </ButtonGroup>
+                    </div>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Unvertraeglichkeiten</Form.Label>
+                    <div className="d-grid">
+                        <ButtonGroup className="flex-wrap">
+                            {checks}
+                        </ButtonGroup>
+                    </div>
+                </Form.Group>
+                <div className="d-grid mt-2">
+                    <Button variant="primary"
+                            disabled={this.state.name.length === 0}
+                            onClick={this.onAddClicked}>
+                        Erstellen
+                    </Button>
+                </div>
             </div>
-            <div className="d-grid gap-2 mt-2">
-                <ButtonGroup className="flex-wrap">
-                    {checks}
-                </ButtonGroup>
-            </div>
-            <div className="d-grid mt-2">
-                <Button variant="primary"
-                        disabled={this.state.name.length === 0}
-                        onClick={this.onAddClicked}>
-                    Erstellen
-                </Button>
-            </div>
-        </Form.Group>);
+        );
     }
 }
