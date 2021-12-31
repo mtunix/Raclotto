b559a78e0960c1146d7e2cf4f4806aee313fed3f
mtardchild <marius.tuschl@gmail.com>
2021-12-31 02:57:36 +0100

    achievement skeleton

--- back/src/model/database.py
+++ back/src/model/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 # Imports and their order are critical
+from back.src.model.domain.achievement import Achievement
 from back.src.model.domain.base import Base
 from back.src.model.domain.rating import Rating
 
@@ -21,6 +22,7 @@ class Database(SQLiteMixin):
         if not Database._engine:
             Database._engine = create_engine(url)
             event.listen(Database._engine, 'connect', Database._fk_pragma_on_connect)
+            event.listen(Achievement.__table__, 'after_create', Database._add_achievements)
             Base.metadata.create_all(Database._engine)
 
         return Database._engine
@@ -28,7 +30,27 @@ class Database(SQLiteMixin):
     @staticmethod
     def session():
         if not Database._session:
-            Database._session = sessionmaker(Database.engine(), expire_on_commit=False, autocommit=True, autoflush=True)()
+            Database._session = sessionmaker(
+                Database.engine(),
+                expire_on_commit=False,
+                autocommit=True,
+                autoflush=True
+            )()
 
         return Database._session
 
+    @staticmethod
+    def _add_achievements():
+        session = Database.session()
+
+        achievements = [
+            Achievement(
+                title="Eat them all!",
+                description="Alle Zutaten des Raclottos einmal gegessen",
+                value=10
+            )
+        ]
+
+        with session.begin():
+            for achievement in achievements:
+                session.add(achievement)
