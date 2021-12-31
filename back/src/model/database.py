f4ff175b1e00b8421b48361a77e76523234dec82
mtardchild <marius.tuschl@gmail.com>
2021-12-31 04:56:23 +0100

    refactoring

--- back/src/model/database.py
+++ back/src/model/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 
 # Imports and their order are critical
+from back.src.model.default_data import ACHIEVEMENTS, INSULTS
 from back.src.model.domain.achievement import Achievement
 from back.src.model.domain.base import Base
+from back.src.model.domain.insults import Insult
 from back.src.model.domain.rating import Rating
 
 
@@ -23,6 +25,7 @@ class Database(SQLiteMixin):
             Database._engine = create_engine(url)
             event.listen(Database._engine, 'connect', Database._fk_pragma_on_connect)
             event.listen(Achievement.__table__, 'after_create', Database._add_achievements)
+            event.listen(Insult.__table__, 'after_create', Database._add_insults)
             Base.metadata.create_all(Database._engine)
 
         return Database._engine
@@ -41,226 +44,12 @@ class Database(SQLiteMixin):
 
     @staticmethod
     def _add_achievements(target, connection, **kwargs):
-        session = Database.session()
+        with Database.session().begin():
+            for achievement in ACHIEVEMENTS:
+                Database.session().add(achievement)
 
-        achievements = [
-            Achievement(
-                title="Gotta eat'em all!",
-                description="Esse alle Zutaten des Raclottos",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="King of the sauce",
-                description="Esse alle Saucen des Raclottos",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="Stack Overflow!",
-                description="Esse eine Pfanne mit mehr als 10 Zutaten",
-                value=8,
-                hidden=False
-            ),
-            Achievement(
-                title="King of the grill",
-                description="Esse am meisten Pfannen in einem Raclotto",
-                value=20,
-                hidden=False
-            ),
-            Achievement(
-                title="Garbage collector",
-                description="Esse eine Pfanne nach einer 3-stündigen Pause",
-                value=5,
-                hidden=False
-            ),
-            Achievement(
-                title="Happy new Year",
-                description="Esse die erste Pfanne des Jahres",
-                value=6,
-                hidden=True
-
-            ),
-            Achievement(
-                title="Local guide",
-                description="Bewerte 10 Pfannen",
-                value=3,
-                hidden=False
-            ),
-            Achievement(
-                title="Local host",
-                description="Eröffne 10 Raclottos",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="First blood!",
-                description="Esse die erste Pfanne des Raclottos",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="Double kill!",
-                description="Erhalte eine identische Pfanne direkt hintereinander",
-                value=30,
-                hidden=False
-            ),
-            Achievement(
-                title="Second to none",
-                description="Starte eine Pfanne in der letzten Minute des Jahres",
-                value=15,
-                hidden=True
-            ),
-            Achievement(
-                title="Zeit für ein D- D- D- Duell!",
-                description="Beende ein Raclotto mit 2 Racleuren",
-                value=8,
-                hidden=True
-            ),
-            Achievement(
-                title="Connoisseur",
-                description="Füge eine nie dagewesene Zutat hinzu",
-                value=3,
-                hidden=False
-            ),
-            Achievement(
-                title="Last pan standing",
-                description="Esse die letzte Pfanne eines Raclottos",
-                value=8,
-                hidden=False
-            ),
-            Achievement(
-                title="Easter egg",
-                description="Esse eine Pfanne mit Ei an Ostern",
-                value=50,
-                hidden=False
-            ),
-            Achievement(
-                title="Vanilla",
-                description="",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="Pandler",
-                description="Nimm an 2 Raclottos innerhalb von 24h teil",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="Wizard",
-                description="Schätze die richtige Anzahl der Pfannen eines Raclottos",
-                value=35,
-                hidden=False
-            ),
-            Achievement(
-                title="Survival of the fittest",
-                description="Esse die schlecht bewerteste Pfanne",
-                value=10,
-                hidden=False
-            ),
-            Achievement(
-                title="Raclottonormalverbraucher",
-                description="Esse eine Pfanne mit Käse und Kartoffel",
-                value=12,
-                hidden=False
-            ),
-            Achievement(
-                title="verPant",
-                description="Mache deine erste Pfanne eine Stunde nach Start des Raclottos",
-                value=5,
-                hidden=True
-            ),
-            Achievement(
-                title="Pantastic",
-                description="Erstelle die best bewerteste Pfanne eines Raclottos",
-                value=8,
-                hidden=False
-            ),
-            Achievement(
-                title="Steakholder",
-                description="Esse 5 Pfannen mit Fleisch",
-                value=5,
-                hidden=False
-            ),
-            Achievement(
-                title="Biotonne",
-                description="Esse 5 vegane Pfannen",
-                value=5,
-                hidden=False
-            ),
-            Achievement(
-                title="Pan sexual",
-                description="Bewerte deine Pfannen mit durchschnittlich 4+",
-                value=8,
-                hidden=True
-            ),
-            Achievement(
-                title="JaPan",
-                description="Esse eine Pfanne mit ausschließlich Fisch",
-                value=12,
-                hidden=False
-            ),
-            Achievement(
-                title="Pan-O-Rama",
-                description="Esse eine Pfanne mit Butter",
-                value=5,
-                hidden=False
-            ),
-            Achievement(
-                title="Panini",
-                description="Esse eine Pfanne mit Teig",
-                value=6,
-                hidden=False
-            ),
-            Achievement(
-                title="Panda",
-                description="Esse eine Pfanne mit 5 Gemüsezutaten und sonst nichts",
-                value=11,
-                hidden=False
-            ),
-            Achievement(
-                title="Pan-nic",
-                description="Starte ein Raclotto unter freim Himmel",
-                value=15,
-                hidden=False
-            ),
-        ]
-        insult = [
-            Insult(
-                title="5-Minuten Terrinen Kocher"
-            ),
-            Insult(
-                title="Rabingospieler"
-            ),
-            Insult(
-                title="Termomixer"
-            ),
-            Insult(
-                title="Nichtsnutz"
-            ),
-            Insult(
-                title="Unkreativling"
-            ),
-            Insult(
-                title="BZgA Beratungsstelle"
-            ),
-            Insult(
-                title="Panner"
-            ),
-            Insult(
-                title="Langweiler"
-            ),
-            Insult(
-                title="Spielverderber"
-            ),
-            Insult(
-                title="Lauch"
-            )
-        ]
-
-        with session.begin():
-            for achievement in achievements:
-                session.add(achievement)
-            for insult in insults:
-                session.add(insult)
+    @staticmethod
+    def _add_insults(target, connection, **kwargs):
+        with Database.session().begin():
+            for insult in INSULTS:
+                Database.session().add(insult)
