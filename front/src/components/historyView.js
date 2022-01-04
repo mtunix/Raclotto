937f2e69b38013fec5baaec3138f107ff224b6c8
mtardchild <marius.tuschl@gmail.com>
2022-01-04 15:55:34 +0100

    ui improvements

--- front/src/components/historyView.js
+++ front/src/components/historyView.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Api} from "../lib/api";
-import {Accordion, ListGroup} from "react-bootstrap";
+import {Accordion, Button, Col, ListGroup, Row} from "react-bootstrap";
 import Rating from "react-rating";
 import {VectorGraphics} from "../lib/vectorGraphics";
 
@@ -40,6 +40,23 @@ export class HistoryView extends React.Component {
         });
     };
 
+    getTags(ingredient) {
+        return (<div>
+            <Button variant="secondary" style={{fontSize: "0.7rem"}} size="sm"
+                    hidden={!ingredient.vegan} className={"mb-1"}>Vegan</Button>{' '}
+            <Button variant="secondary" style={{fontSize: "0.7rem"}} size="sm"
+                    hidden={!ingredient.vegetarian} className={"mb-1"}>Vegetarisch</Button>{' '}
+            <Button variant="secondary" style={{fontSize: "0.7rem"}} size="sm"
+                    hidden={!ingredient.histamine} className={"mb-1"}>Histamin</Button>{' '}
+            <Button variant="secondary" style={{fontSize: "0.7rem"}} size="sm"
+                    hidden={!ingredient.gluten} className={"mb-1"}>Gluten</Button>{' '}
+            <Button variant="secondary" style={{fontSize: "0.7rem"}} size="sm"
+                    hidden={!ingredient.lactose} className={"mb-1"}>Lactose</Button>{' '}
+            <Button variant="secondary" style={{fontSize: "0.7rem"}} size="sm"
+                    hidden={!ingredient.fructose} className={"mb-1"}>Fructose</Button>{' '}
+        </div>);
+    }
+
     render() {
         let getVariant = (type) => type === 1 ? "primary" : "secondary";
         let pans = this.state.pans.map((pan, i) =>
@@ -50,11 +67,38 @@ export class HistoryView extends React.Component {
                     {this.getRating(pan.id, pan.rating)}
                 </Accordion.Header>
                 <Accordion.Body>
-                    <ListGroup key={this.props.ingredients} className="mb-2">
-                        {pan.ingredients.map((ingredient) =>
-                            <ListGroup.Item variant={getVariant(ingredient.type)}>{ingredient.name}</ListGroup.Item>
-                        )}
-                    </ListGroup>
+                    <Row>
+                        <Col sm>
+                            <h5>Zutaten</h5>
+                            <ListGroup key={this.props.ingredients} className="mb-2">
+                                {pan.ingredients.filter(ingredient => ingredient.type === 1).map((ingredient) =>
+                                    <ListGroup.Item variant={getVariant(ingredient.type)}>
+                                        <Row>
+                                            <Col>
+                                                <span style={{fontSize: "1rem"}}>{ingredient.name}</span>
+                                            </Col>
+                                        </Row>
+                                        {this.getTags(ingredient)}
+                                    </ListGroup.Item>
+                                )}
+                            </ListGroup>
+                        </Col>
+                        <Col sm>
+                            <h5>Saucen</h5>
+                            <ListGroup key={this.props.ingredients} className="mb-2">
+                                {pan.ingredients.filter(ingredient => ingredient.type === 2).map((ingredient) =>
+                                    <ListGroup.Item variant={getVariant(ingredient.type)}>
+                                        <Row>
+                                            <Col>
+                                                <span style={{fontSize: "1rem"}}>{ingredient.name}</span>
+                                            </Col>
+                                        </Row>
+                                        {this.getTags(ingredient)}
+                                    </ListGroup.Item>
+                                )}
+                            </ListGroup>
+                        </Col>
+                    </Row>
                     <span style={{fontStyle: "italic", fontSize: "smaller"}} className="px-3">verspeist um {pan.timestamp}</span>
                 </Accordion.Body>
             </Accordion.Item>
