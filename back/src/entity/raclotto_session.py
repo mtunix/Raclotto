56ef9ba372781acd53690b2821ab084859177050
mtardunix <marius.tuschl@wwv.de>
2023-09-07 09:02:42 +0200

    Setting generated uuid4 as default value for session key

--- back/src/entity/raclotto_session.py
+++ back/src/entity/raclotto_session.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 
 from sqlalchemy import Column, Integer, String, DateTime, Boolean
@@ -10,7 +11,7 @@ class RaclottoSession(SerializableMixin, BaseModel):
     __tablename__ = "session"
 
     id = Column(Integer, primary_key=True)
-    key = Column(String, nullable=False, unique=True)
+    key = Column(String, nullable=False, unique=True, default=uuid.uuid4())
     name = Column(String, nullable=False)
     timestamp = Column(DateTime, nullable=False, default=datetime.now())
     active = Column(Boolean, nullable=False, default=True)
