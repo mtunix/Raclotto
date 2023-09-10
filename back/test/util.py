01fd431360876e2615b8e2c4520479bf313676c2
mtunix <marius.tuschl@googlemail.com>
2023-09-11 00:03:50 +0200

    Added second test session

--- back/test/util.py
+++ back/test/util.py
@@ -53,6 +53,7 @@ class DBTest(unittest.TestCase):
         session = RaclottoSession(key=session_id, name="Test Session")
         user = User(name="Berta", password="123123")
         app.session.add(session)
+        app.session.add(RaclottoSession(key="123", name="Test Session 2"))
         app.session.add(user)
         app.session.commit()
 
